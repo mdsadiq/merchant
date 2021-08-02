3a2e796d8935257fc929ba5d6e91e15ea74db05e
mdsadiq <mdsadiq6692@gmail.com>
2021-08-02 19:33:09 +0530

    add repo env and fix credits issue

--- pages/credits.js
+++ pages/credits.js
@@ -1,4 +1,4 @@
-export default CreditsPage = () => {
+export default function CreditsPage() {
   return(
     <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
   )
