ffc3263d68ae36c3e43c2f762673bfde8a985d6d
mdsadiq <mdsadiq6692@gmail.com>
2021-08-03 17:34:59 +0530

    change error to warn in lhci file

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -8,10 +8,10 @@ module.exports = {
     "assert": {
       "preset": "lighthouse:recommended",
       "assertions": {
-        "categories:performance": [ "error", {"minScore": 0.8 } ],
-        "categories:accessibility": [ "error", {"minScore": 0.8 } ],
-        "categories:best-practices": [ "error", {"minScore": 0.8 } ],
-        "categories:seo": [ "error", {"minScore": 0.8 } ],
+        "categories:performance": [ "warn", {"minScore": 0.8 } ],
+        "categories:accessibility": [ "warn", {"minScore": 0.8 } ],
+        "categories:best-practices": [ "warn", {"minScore": 0.8 } ],
+        "categories:seo": [ "warn", {"minScore": 0.8 } ],
         // "first-contentful-paint": [
         //   "warn",
         //   {
