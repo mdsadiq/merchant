0b6942d2c533a7f44f5d273a9e9ee499635b8392
mdsadiq <mdsadiq6692@gmail.com>
2021-08-05 19:18:21 +0530

    json to js

---
+++ .lighthouserc.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: "npm run start",
+      startServerReadyTimeout: 15000,
+      url: [
+        "http://localhost:3000/",
+        "http://localhost:3000/day/"
+      ],
+      numberOfRuns: 2,
+    },
+    assert: {
+      // preset: "lighthouse:no-pwa",
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.8 }],
+        "categories:accessibility": ["warn", { minScore: 0.8 }],
+        "categories:best-practices": ["warn", { minScore: 0.8 }],
+        "categories:seo": ["warn", { minScore: 0.8 }],
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
