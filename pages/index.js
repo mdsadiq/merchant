78eea4a43bab12fed2745379b6c998a7bc63ef3e
mdsadiq <mdsadiq6692@gmail.com>
2021-08-05 10:06:27 +0530

    update classname in footer and fix links in index.js

--- pages/index.js
+++ pages/index.js
@@ -25,9 +25,9 @@ export default function IndexPage() {
         </p>
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <Link href="/product/ckdu49mfc0h070102jgprxnj0">
+          <Link href="/product/ckdu49mfc0h070102jgprxnj0" as="/product/ckdu49mfc0h070102jgprxnj0">
+
             <a
-              href="#"
               className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
             >
               <h3 className="text-2xl font-bold">Product 221</h3>
@@ -36,22 +36,13 @@ export default function IndexPage() {
           </Link>
           <Link href="/day">
             <a
-              href="#"
               className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
             >
               <h3 className="text-2xl font-bold">Hello Day</h3>
               <p className="mt-4 text-xl">Go to the intro day</p>
             </a>
           </Link>
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+
         </div>
       </main>
 
